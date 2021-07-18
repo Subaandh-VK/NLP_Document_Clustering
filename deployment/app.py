2157fbee77a0579742fa5480e1bae3dac642a7d8
subaandh-VK <subaandh@gmail.com>
2021-07-18 18:09:41 +0200

    Implemented K means model with deployement

---
+++ deployment/app.py
@@ -0,0 +1,48 @@
+import json
+
+import pandas as pd
+from flask import Flask, request, render_template
+from joblib import load
+
+app = Flask(__name__)
+
+# Import the model
+model = load('./k_means_clustering.pkl')
+vectorizer = load('./tfidf_vectorizer.pkl')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    form_dict = request.form.to_dict()
+
+    if request.files['json_file']:
+        print(request.files.values())
+        df = pd.read_json(request.files['json_file'])
+        clause_list = df['clauses'].to_list()
+        clause_content = ''.join(str(val) for val in clause_list)
+        print(clause_content)
+
+        x = vectorizer.transform([clause_content])
+        prediction = model.predict(x)
+
+        prediction = 'Prediction for document is cluster:' + str(prediction)
+
+    elif form_dict.get('file_content') != '':
+        x = vectorizer.transform([form_dict.get('file_content')])
+        prediction = model.predict(x)
+
+        prediction = 'Prediction for text is cluster:' + str(prediction)
+
+    else:
+        prediction = 'Unable to predict'
+
+    return render_template('index.html', prediction=f" Result : {prediction}")
+
+
+if __name__ == '__main__':
+    app.run()
